05255d03a4bbbdf52bccf15c9af0324c3e88b942
Jefferson Nascimento <jefferson.nascimento3@fatec.sp.gov.br>
2016-08-15 23:29:16 -0300

    Corrige solução do Problema A

--- Problema A - Marqueenho/marquee.java
+++ Problema A - Marqueenho/marquee.java
@@ -1,37 +1,58 @@
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class marquee {
-
+    public static String geraEspacos(int tam) {
+        return new String(new char[tam]).replace("\0", " ");
+    }
+    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
-        int marqueeLen, textoLen, tempoDecorrido;
-        String texto, espacoEmBranco, textoFinal, aux;
+
+        int tamMarquee, tamTexto, tempoDecorrido, tamPrimeiraPalavra;
+        String texto, textoFinal, aux;
         ArrayList<String> marquee;
+        String separadores = "[ ,.:;!?]";
+        Pattern pattern = Pattern.compile(separadores);
+        Matcher matcher;
 
         while (scan.hasNext()) {
-            marqueeLen = scan.nextInt();
-            textoLen = scan.nextInt();
+            tamMarquee = scan.nextInt();
+            tamTexto = scan.nextInt();
             scan.nextLine(); // Consome o \n do fim da linha, para ler corretamente a linha do texto
             texto = scan.nextLine();
             tempoDecorrido = scan.nextInt();
+            
+            matcher = pattern.matcher(texto); // Procura os separadores no texto
 
-            // Se o marquee for maior que o texto
-            if (marqueeLen > textoLen) {
-                // Gera uma String somente com espaços em branco para completar o texto no marquee
-                espacoEmBranco = new String(new char[marqueeLen - 1]).replace("\0", " ");
-
-                // Adiciona o espaço em branco ao final do texto
-                texto += espacoEmBranco;
+            if (matcher.find()) {
+                // Caso encontre, pega o índice do primeiro match pra usar como tamanho da primeira palavra
+                tamPrimeiraPalavra = matcher.start();
+            } else {
+                // Caso não encontre, não há separadores, então só há uma palavra
+                tamPrimeiraPalavra = tamTexto;
+            }
+            
+            // Se o texto for menor que o marquee
+            if (tamTexto < tamMarquee) {
+                // Completa o texto com espaços em branco até chegar ao tamanho do marquee
+                texto += geraEspacos(tamMarquee - tamTexto);
+            }
+            
+            // Se retirando a primeira palavra do texto, ele fica menor que o marquee
+            if (texto.length() - tamPrimeiraPalavra < tamMarquee) {
+                // Adiciona o espaço em branco ao final do texto equivalente ao tamanho da primeira palavra
+                texto += geraEspacos(tamPrimeiraPalavra - 1);
             }
 
             // Transforma o texto em um ArrayList de caracteres
             marquee = new ArrayList<>(Arrays.asList(texto.split("")));
 
-            // O estado inicial do marquee é atingido no instante equivalento ao tamanho do texto + 1
-            if (tempoDecorrido > marqueeLen) {
-                tempoDecorrido = tempoDecorrido % (texto.length() + 1);
+            // O estado inicial do marquee se repete em determinado tempo
+            if (tempoDecorrido > tamMarquee) {
+                tempoDecorrido = (tempoDecorrido % (texto.length())) + 1;
             }
 
             // Corre o "tempo" e faz as trocas de posição
@@ -46,7 +67,7 @@ class marquee {
 
             // Compõe a String de saída
             textoFinal = "";
-            for (int i = 0; i < marqueeLen; i++) {
+            for (int i = 0; i < tamMarquee; i++) {
                 textoFinal += marquee.get(i);
             }
 
